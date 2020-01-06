20dd295bc8f9c37d73aeff34121ef24cb75edac0
Anderson <anderson@flexpro.com.br>
2020-01-06 14:46:15 -0300

    index and list method added list layout file added

--- src/AppBundle/Controller/GenusNoteController.php
+++ src/AppBundle/Controller/GenusNoteController.php
@@ -12,5 +12,25 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class GenusNoteController extends Controller
 {
+    /**
+     * @Route("/list", name="notes_list")
+     * @noinspection PhpUnused
+     */
+    public function listAction()
+    {
+        $notes = $this->getDoctrine()->getRepository('AppBundle:GenusNote')->findAll();
 
+        $this->render('Notes/list.html.twig', array(
+            'notes' => $notes
+        ));
+    }
+
+    /**
+     * @Route("/")
+     * @noinspection PhpUnused
+     */
+    public function indexAction()
+    {
+        return $this->redirectToRoute('notes_list');
+    }
 }
